7af5ce11a76b4964e7e05a5286bd8b718025cbfe
Theerapat.Demo <theerapat.demo@gmail.com>
2025-11-13 20:33:26 +0700

    handle script content-type, response time, body length

--- k6-api/smoke_test.js
+++ k6-api/smoke_test.js
@@ -1,7 +1,15 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 
 export default function () {
-  http.get('http://srv946485.hstgr.cloud:3000/api/inventory');
+  const res = http.get("http://srv946485.hstgr.cloud:3000/api/inventory");
   sleep(1);
+
+  check(res, {
+    status: (res) => res.status === 200,
+    "content-type": (res) =>
+      res.headers["Content-Type"] === "application/json; charset=utf-8",
+    "response-time": (res) => res.timings.duration < 200,
+    "body-length": (res) => res.body.length > 0,
+  });
 }
