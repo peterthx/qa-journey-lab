76e845a3d5b067bb54f86be2ae9ab7c9e5914059
Theerapat-ngoipoothon <71537101+peterthx@users.noreply.github.com>
2025-11-15 15:47:37 +0700

    del file not use and format code

--- playwright-web/tests/test_login_form.spec.ts
+++ playwright-web/tests/test_login_form.spec.ts
@@ -1,32 +1,34 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Login and Logout flow', async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
+test("Login and Logout flow", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
 
-    // Login
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+  // Login
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
 
-    // login success
-    await expect(page).toHaveURL(/inventory.html/);
+  // login success
+  await expect(page).toHaveURL(/inventory.html/);
 
-    // Open menu and logout
-    await page.locator('#react-burger-menu-btn').click();
-    await page.locator('[data-test="logout-sidebar-link"]').click();
+  // Open menu and logout
+  await page.locator("#react-burger-menu-btn").click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
 
-    // logout success (back to login page)
-    await expect(page).toHaveURL('https://www.saucedemo.com/');
+  // logout success (back to login page)
+  await expect(page).toHaveURL("https://www.saucedemo.com/");
 });
 
-test('Login with locked out user', async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
+test("Login with locked out user", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
 
-    await page.locator('[data-test="username"]').fill('locked_out_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="username"]').fill("locked_out_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
 
-    // Assert error message appears
-    const errorMessage = page.locator('[data-test="error"]');
-    await expect(errorMessage).toContainText('Sorry, this user has been locked out.');
+  // Assert error message appears
+  const errorMessage = page.locator('[data-test="error"]');
+  await expect(errorMessage).toContainText(
+    "Sorry, this user has been locked out."
+  );
 });
