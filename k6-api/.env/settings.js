e9ae137b1d320fc9b6cff84a63dd06b77aa469dc
Theerapat-ngoipoothon <71537101+peterthx@users.noreply.github.com>
2025-11-26 20:29:00 +0700

    config script file test k6 #3

---
+++ k6-api/.env/settings.js
@@ -0,0 +1,10 @@
+export const BASE_URL = {
+  ENDPOINT: 'http://srv946485.hstgr.cloud:3000'
+};
+
+export const SERVICE = {
+  GET_ALL_PRODUCTS: "/api/products",
+  POST_NEW_SUPPLIER: "/api/suppliers",
+  DEL_SUPPLIER: "/api/suppliers",
+  GET_ALL_SUPPLIERS: "/api/suppliers"
+};
