8b26c8ed4178324b06554c8792a60d74fd1f399d
Theerapat-ngoipoothon <71537101+peterthx@users.noreply.github.com>
2025-12-05 22:08:23 +0700

    del folder not use

--- playwright-api/playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  use: {
-    baseURL: 'http://srv946485.hstgr.cloud:3000',
-  },
-  testDir: 'tests',
-  timeout: 30000
-});
