b1613d159c64ac63cbccbc37acdc712cdbb94ddd
Theerapat-ngoipoothon <71537101+peterthx@users.noreply.github.com>
2025-12-13 21:19:00 +0700

    fix file cartPage

--- playwrightWebApp/pages/cart/CartPage.ts
+++ playwrightWebApp/pages/cart/CartPage.ts
@@ -4,7 +4,7 @@ export class CartPage {
     readonly page: Page;
     readonly checkoutButton: Locator;
     readonly continueShoppingButton: Locator;
- 
+
     constructor(page: Page) {
         this.page = page;
         this.checkoutButton = page.locator('[data-test="checkout"]');
