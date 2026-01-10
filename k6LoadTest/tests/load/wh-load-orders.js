49b76bcb1ff9da7bf9cfaf322c695ac21c137704
Theerapat-ngoipoothon <71537101+peterthx@users.noreply.github.com>
2026-01-10 12:13:24 +0700

    open fix file load test in project

--- k6LoadTest/tests/load/wh-load-orders.js
+++ k6LoadTest/tests/load/wh-load-orders.js
@@ -8,17 +8,17 @@ import {
   randomQuantity,
 } from "../../utils/helpers.js";
 
-// export const options = {
-//   stages: [
-//     { duration: "1m", target: 3 },
-//     { duration: "5m", target: 3 },
-//     { duration: "1m", target: 0 },
-//   ],
-//   thresholds: {
-//     http_req_duration: ["p(95)<500"], // 95% of requests should be below 500ms
-//     http_req_failed: ["rate<0.01"], // Less than 1% of requests should fail
-//   },
-// };
+export const options = {
+  stages: [
+    { duration: "1m", target: 3 },
+    { duration: "5m", target: 3 },
+    { duration: "1m", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<500"], // 95% of requests should be below 500ms
+    http_req_failed: ["rate<0.01"], // Less than 1% of requests should fail
+  },
+};
 
 export function createOrderPayload() {
   return {
