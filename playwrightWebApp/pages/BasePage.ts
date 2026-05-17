9cfb9bccb5bc22e96e6f8e1f95d9e527b1a02d61
theerapat-ngoipoothon <theerapat.demo@gmail.com>
2026-05-17 14:45:46 +0700

    refactor: improve class BasePage in class

--- playwrightWebApp/pages/BasePage.ts
+++ playwrightWebApp/pages/BasePage.ts
@@ -1,12 +1,10 @@
 import { Page } from "@playwright/test";
 
 export class BasePage {
-  protected page: Page;
-
-  constructor(page: Page) {
+  constructor(protected page: Page) {
     this.page = page;
   }
-  
+
   async navigate(path: string) {
     await this.page.goto(path);
   }
